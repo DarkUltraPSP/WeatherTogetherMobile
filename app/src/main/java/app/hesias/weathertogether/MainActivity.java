af15c71cfb486aa643e20d0ecd03137456e0d53f
DarkUltraPSP <darkultrapsp.mc@gmail.com>
2023-03-11 13:21:02 +0100

    Search postal code by city name

--- app/src/main/java/app/hesias/weathertogether/MainActivity.java
+++ app/src/main/java/app/hesias/weathertogether/MainActivity.java
@@ -13,8 +13,13 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     Button search;
@@ -34,27 +39,45 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick (View view) {
                 // Instantiate the RequestQueue.
-                RequestQueue queue =  Volley.newRequestQueue(this);
-                String url = "https://www.google.com";
+                RequestQueue queue =  Volley.newRequestQueue(MainActivity.this);
+                String url = String.format("https://geo.api.gouv.fr/communes?nom=%s&fields=nom,codesPostaux,centre&format=json&geometry=centrew", city.getText().toString());
 
-                // Request a string response from the provided URL.
-                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                // Display the first 500 characters of the response string.
-                                textView.setText("Response is: " + response.substring(0,500));
-                            }
-                        }, new Response.ErrorListener() {
+                JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url,null, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            JSONObject cityInfo = response.getJSONObject(0);
+                            String postCode = cityInfo.getString("codesPostaux");
+                            Toast.makeText(MainActivity.this, postCode, Toast.LENGTH_SHORT).show();
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        textView.setText("That didn't work!");
+                        Toast.makeText(MainActivity.this, "Error" + error, Toast.LENGTH_SHORT).show();
                     }
                 });
 
+                // Request a string response from the provided URL.
+//                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+//                        new Response.Listener<String>() {
+//                            @Override
+//                            public void onResponse(String response) {
+//                                // Display the first 500 characters of the response string.
+//                                Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+//                            }
+//                        }, new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        Toast.makeText(MainActivity.this, "Error" + error, Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+
                 // Add the request to the RequestQueue.
-                queue.add(stringRequest);
-                Toast.makeText(MainActivity.this, "Searching for" + city.getText().toString(), Toast.LENGTH_SHORT).show();
+                queue.add(request);
+//                Toast.makeText(MainActivity.this, "Searching for" + city.getText().toString(), Toast.LENGTH_SHORT).show();
             }
         });
     }
